e24234bce79ba5d144fe0b5fe0453acc8f947b8f
miniluz <miniluz@miniluz.dev>
2026-09-18 12:54:25 +0200

    feat: :sparkles: Make background nice even on big screens

--- packages/frontend/src/lib/components/background.tsx
+++ packages/frontend/src/lib/components/background.tsx
@@ -5,7 +5,7 @@ export function Background({
   return (
     <main
       className={`min-h-screen bg-cover text-black centered-flex flex-col gap-5
-        before:absolute before:inset-0 before:bg-[url(/images/background.jpg)] before:bg-cover before:bg-center before:-z-10
+        before:absolute before:inset-0 before:bg-[url(/images/Background.jpg)] before:bg-size-[600px] before:bg-center before:-z-10
         before:brightness-130
         before:saturate-40
         ${className}`}

843fbd9092fd2d7c4b200342f2ff39694979825a
Emilio-115 <emiliovc2016@gmail.com>
2026-09-26 13:32:31 +0200

    feat: :wrench: add env variables for valkey config

--- packages/backend/src/config.ts
+++ packages/backend/src/config.ts
@@ -4,6 +4,9 @@ export const configSchema = z.object({
   FRONTEND_URL: z.url(),
   OTLP_TRACE_EXPORTER_URL: z.url(),
   LOKI_URL: z.url(),
+  VALKEY_HOST: z.string(),
+  VALKEY_PORT: z.coerce.number(),
+  VALKEY_PASSWORD: z.string(),
 });
 
 export type Config = z.infer<typeof configSchema>;
